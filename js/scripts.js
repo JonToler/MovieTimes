e04ffbfb0c73753cd256d91b9d660ae640bc2486
Joshua Casuccio and Jon Toler <joshua@>
2016-08-23 11:01:20 -0700

    initial content, files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Business Logic
+function ticket(ticket, age, time, movie){
+  this.ticket = ticket;
+  this.age = age;
+  this.time = time;
+  this.movie = movie;
+}
+
+
+// UI Logic
+$(document).ready(function() {
+
+  });
