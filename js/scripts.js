f72409ea65bb4e7af18db4c093fa9b2ab4007017
Joshua Casuccio and Jon Toler <jon@>
2016-08-23 11:51:50 -0700

    setup some UI and forms

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,20 @@ function ticket(ticket, age, time, movie){
   this.age = age;
   this.time = time;
   this.movie = movie;
+  this.ticetPrice = [""];
 }
-
-
+var ticketPriceChild = "$2";
+var ticketPriceSenior = "$3";
+var ticketPriceAdult = "$4";
+ticket.prototype.prices = function () {
+  this.ticket.push(ticketPriceChild);
+}
 // UI Logic
 $(document).ready(function() {
+$("form#movieForm").submit(function(event){
+  event.preventDefault();
+  var movieIn = $("#SpaceOdd").val();
+  alert(ticketPriceChild);
 
   });
+});
